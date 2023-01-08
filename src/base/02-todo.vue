3c1f85f3426bc1423b5412f445e98b219e544642
Starszhangtao <3054296577@qq.com>
2023-01-08 22:35:18 +0800

    frist commit

--- src/base/02-todo.vue
+++ src/base/02-todo.vue
@@ -13,10 +13,6 @@
 import { reactive ,ref  } from '@vue/reactivity'
   export  default {
       setup(){
-        //可以定义多个reactive 只要return出去就行
-        // const mylist=reactive([])//这个可以直接允许定义，数字字符串不可以
-        // const obj2=reactive('')//不可以
-        // const obj3=reactive(1111)//不可以
        const obj=reactive({
         mytext:'',
         datalist:[],
